81391e8d6b96d5968065e69a5a521a2ad2494bcb
fawda123 <mbafs2012@gmail.com>
2020-02-11 11:53:18 -0500

    package skeleton with raw data

---
+++ rawdat.R
@@ -0,0 +1,17 @@
+#' Raw data from San Francisco Estuary (South Bay)
+#'
+#' Raw data from San Francisco Estuary (South Bay)
+#'
+#' @format A \code{data.frame} object with 37301 rows and 8 columns
+#' \describe{
+#'   \item{date}{Date}
+#'   \item{station}{int}
+#'   \item{param}{chr}
+#'   \item{value}{num}
+#'   \item{doy}{num}
+#'   \item{dec_time}{num}
+#'   \item{yr}{num}
+#'   \item{mo}{Ord.factor}
+#' }
+#' @family utilities
+"rawdat"
