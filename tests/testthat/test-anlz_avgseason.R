c68b200c368cfff7d7ce16be2fb76407f031c68d
fawda123 <mbafs2012@gmail.com>
2020-03-19 13:00:25 -0400

    added anlz_avgseason

---
+++ tests/testthat/test-anlz_avgseason.R
@@ -0,0 +1,36 @@
+test_that("Checking anlz_avgseason", {
+  
+  # use previously fitted list of models
+  tomod <- rawdat %>%
+    dplyr::filter(station %in% 32) %>%
+    dplyr::filter(param %in% 'chl')
+  result <- anlz_avgseason(tomod, trans = 'log10', doystr = 90, doyend = 180) 
+  
+  expect_equal(nrow(result), 112)
+  
+})
+
+test_that("Checking , list input", {
+  
+  tomod <- rawdat %>%
+    dplyr::filter(station %in% 32) %>%
+    dplyr::filter(param %in% 'chl')
+  
+  # use previously fitted list of models
+  trans <- 'boxcox'
+  mods <- list(
+    gam0 = anlz_gam(tomod, mod = 'gam0', trans = trans),
+    gam1 = anlz_gam(tomod, mod = 'gam1', trans = trans),
+    gam2 = anlz_gam(tomod, mod = 'gam2', trans = trans)
+  )
+  result <- anlz_avgseason(mods = mods, doystr = 90, doyend = 180) 
+  
+  expect_equal(nrow(result), 84)
+  
+})
+
+test_that("Checking  error, insufficient inputs", {
+  
+  expect_error(anlz_avgseason(doystr = 90, doyend = 180))
+  
+})
