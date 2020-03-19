15d37cce3397e439ae7e830736581660232788d5
fawda123 <mbafs2012@gmail.com>
2020-03-19 13:30:48 -0400

    added anlz_mixmeta

---
+++ R/anlz_mixmeta.R
@@ -0,0 +1,43 @@
+#' Fit a mixed meta-analysis regresion model of trends
+#' 
+#' Fit a mixed meta-analysis regresion model of trends
+#' 
+#' @param avgseason output from \code{\link{anlz_avgseason}}
+#' @param yrstr numeric for starting year
+#' @param yrend numeric for ending year
+#'
+#' @return A list of \code{\link[mixmeta]{mixmeta}} fitted model objects
+#' @export
+#'
+#' @examples
+#' trans <- 'boxcox'
+#' mods <- list(
+#'   gam0 = anlz_gam(tomod, mod = 'gam0', trans = trans),
+#'   gam1 = anlz_gam(tomod, mod = 'gam1', trans = trans), 
+#'   gam2 = anlz_gam(tomod, mod = 'gam2', trans = trans)
+#'   )
+#' avgseason <- anlz_avgseason(mods = mods, doystr = 90, doyend = 180)
+#' anlz_mixmeta(avgseason, yrstr = 2000, yrend = 2017)
+anlz_mixmeta <- function(avgseason, yrstr, yrend){
+
+  # input
+  totrnd <- avgseason %>% 
+    dplyr::mutate(S = se^2) %>% 
+    dplyr::filter(yr %in% seq(yrstr, yrend))
+
+  out <- totrnd %>%
+    dplyr::group_by(model) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(
+      mixmod = purrr::map(data, function(x){
+        
+        mixmeta::mixmeta(predicted ~ yr, S = S, random = ~1|yr, data = x, method = 'reml')
+        
+      })
+    ) %>% 
+    dplyr::select(-data) %>% 
+    tibble::deframe()
+    
+  return(out)
+
+}
---
+++ tests/testthat/test-anlz_mixmeta.R
@@ -0,0 +1,14 @@
+test_that("Checking anlz_mixmeta", {
+
+  trans <- 'boxcox'
+  mods <- list(
+    gam0 = anlz_gam(tomod, mod = 'gam0', trans = trans),
+    gam1 = anlz_gam(tomod, mod = 'gam1', trans = trans),
+    gam2 = anlz_gam(tomod, mod = 'gam2', trans = trans)
+    )
+  avgseason <- anlz_avgseason(mods = mods, doystr = 90, doyend = 180)
+  result <- anlz_mixmeta(avgseason, yrstr = 2000, yrend = 2017)
+  
+  expect_is(result[[1]], 'mixmeta')
+  
+})
