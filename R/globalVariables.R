9c8f38ca2e77dc5def93a18e67c60995aea1b6ce
fawda123 <mbafs2012@gmail.com>
2021-01-27 09:19:18 -0500

    man build

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,6 +1,6 @@
 globalVariables(c('%>%', '.', 'AIC', 'S', 'annvalue', 'avg', 'bl', 'avg', 'lwr', 'upr', 'cont_year', 'bt_avg', 
                   'bt_lwr', 'bt_upr', 'day', 'doy', 'month', 'pval', 'se', 'trans', 'value', 'xmax', 'xmin', 
-                  'xval', 'yr', 'yrcoef'))
+                  'xval', 'yr', 'yrcoef', 'appr_yrcoef', 'yrcoef_lwr', 'yrcoef_upr'))
                 
-#' @importFrom stats AIC as.formula coef coefficients na.exclude na.omit pt predict qnorm resid sd update
+#' @importFrom stats AIC as.formula coef coefficients lm na.exclude na.omit pt predict qnorm resid sd update
 NULL
