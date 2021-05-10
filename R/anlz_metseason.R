02261ed9697d65a02e35d1d04d42e50b72a2cd5d
fawda123 <mbafs2012@gmail.com>
2021-05-10 14:10:50 -0400

    added anlz_metseason as generic function for any seasonal metric

---
+++ R/anlz_metseason.R
@@ -0,0 +1,113 @@
+#' Extract period (seasonal) metrics from fitted GAM
+#' 
+#' Extract period (seasonal) metrics from fitted GAM
+#' 
+#' @param mod input model object as returned by \code{\link{anlz_gam}}
+#' @param metfun function input for metric to calculate, e.g., \code{mean}, \code{var}, \code{max}, etc
+#' @param doystr numeric indicating start Julian day for extracting averages
+#' @param doyend numeric indicating ending Julian day for extracting averages
+#' @param nsim numeric indicating number of random draws for simulating uncertainty
+#' @param ... additional arguments pass to \code{metfun}, e.g., \code{na.rm = TRUE)}
+#'
+#' @return A data frame of period metrics
+#' @export
+#' 
+#' @details This function estimates a metric of interest for a given seasonal period each year using results from a fitted GAM (i.e., from \code{\link{anlz_gam}}).  The estimates are based on the predicted values for each seasonal period, with uncertainty of the metric based on repeated sampling of the predictions following uncertainty in the model coefficients.
+#'
+#' This function will converge to identical results provided by \code{\link{anlz_avgseaon}} if \code{metfun = mean} and \code{nsim} is very large.
+#' 
+#' @concept analyze
+#' 
+#' @examples
+#' library(dplyr)
+#' 
+#' # data to model
+#' tomod <- rawdat %>%
+#'   filter(station %in% 34) %>%
+#'   filter(param %in% 'chl')
+#'
+#' mod <- anlz_gam(tomod, trans = 'log10')
+#' anlz_metseason(mod, mean, doystr = 90, doyend = 180, nsim = 100)
+anlz_metseason <- function(mod, metfun = mean, doystr = 1, doyend = 364, nsim = 1e4, ...) {
+  
+  # gam model data
+  gamdat <- mod$model %>% 
+    dplyr::mutate(
+      date = lubridate::date_decimal(cont_year), 
+      date = as.Date(date)
+    )
+
+  # transformation
+  trans <- mod$trans
+  
+  # number of days in seasonal window
+  numDays <- doyend - doystr + 1
+  
+  # prep prediction data
+  dtrng <- range(gamdat$date, na.rm = T)
+  fillData <- data.frame(date = seq.Date(dtrng[1], dtrng[2], by = 'day')) %>% 
+    dplyr::mutate(
+      yr = lubridate::year(date), 
+      doy = lubridate::yday(date),
+      cont_year = lubridate::decimal_date(date)
+    ) %>% 
+    dplyr::filter(doy >= doystr & doy <= doyend) %>% 
+    dplyr::group_by(yr) %>% 
+    dplyr::mutate(
+      dayCounts = n()
+    ) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::filter(dayCounts == numDays) %>% 
+    dplyr::select(-dayCounts)
+  
+  # basis function coefficients and var/cov matrix (uncertainty of coefs)
+  gamcoefs <- coef(mod)
+  vargamcoefs <- mod$Vp
+  
+  # random sim of coefs basedon multivariate normal
+  coefrnd <- rmvn(nsim, gamcoefs, vargamcoefs) 
+  
+  mets <- fillData %>% 
+    dplyr::group_by(yr) %>% 
+    tidyr::nest() %>% 
+    dplyr::mutate(
+      met = purrr::map(data, function(x){
+        
+        Xp <- predict(mod, x, typ = 'lpmatrix') # all basis functions
+        
+        pred <- Xp %*% gamcoefs
+        met <- metfun(pred, ...)
+        
+        sims <- Xp %*% t(coefrnd)
+        unc <- apply(sims, 2, metfun, ...)
+        ses <- sd(unc)
+        
+        out <- data.frame(met = met, se = ses)
+        
+        return(out)
+        
+      })
+    ) %>% 
+    dplyr::select(-data) %>% 
+    dplyr::ungroup() %>% 
+    tidyr::unnest('met')
+  
+  # backtransform, add lwr/upr confidence intervals
+  dispersion <- summary(mod)$dispersion
+  
+  if(trans == 'log10'){
+    mets$bt_lwr <- 10^((mets$met - 1.96 * mets$se) + log(10) * dispersion / 2)
+    mets$bt_upr <- 10^((mets$met + 1.96 * mets$se) + log(10) * dispersion / 2)
+    mets$bt_met <- 10^(mets$met + log(10) * dispersion / 2)
+  }
+  if(trans == 'ident'){
+    mets$bt_met <- mets$met
+    mets$bt_lwr <- mets$met - 1.96 * mets$se
+    mets$bt_upr <- mets$met + 1.96 * mets$se
+  }
+  
+  out <- mets
+  
+  return(out)
+  
+}
