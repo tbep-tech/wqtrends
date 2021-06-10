35e59bb46486a7b86495a605bdbb14f3e6840747
fawda123 <mbafs2012@gmail.com>
2021-06-10 08:50:20 -0400

    removed anlz_avgseason

--- tests/testthat/test-anlz_mixmeta.R
+++ tests/testthat/test-anlz_mixmeta.R
@@ -1,6 +1,6 @@
 test_that("Checking anlz_mixmeta", {
 
-  avgseason <- anlz_avgseason(mod, doystr = 90, doyend = 180)
+  avgseason <- anlz_metseason(mod, doystr = 90, doyend = 180)
   result <- anlz_mixmeta(avgseason, yrstr = 2000, yrend = 2017)
   
   expect_is(result, 'mixmeta')
