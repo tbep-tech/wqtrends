261848e19de6b0d11b039943b4df4d29f035bc0b
fawda123 <mbafs2012@gmail.com>
2021-06-23 12:53:46 -0400

    annual argument to anlz_pred

--- R/anlz_prd.R
+++ R/anlz_prd.R
@@ -3,6 +3,7 @@
 #' Get predicted data from fitted GAMs across period of observation
 #' 
 #' @param mod input model object as returned by \code{\link{anlz_gam}}
+#' @param annual logical indicating if predictions only for the \code{cont_year} smoother are returned
 #'
 #' @return a \code{data.frame} with predictions
 #' @export
@@ -19,7 +20,7 @@
 #'   
 #' mod <- anlz_gam(tomod, trans = 'log10')
 #' anlz_prd(mod)
-anlz_prd <- function(mod) {
+anlz_prd <- function(mod, annual = FALSE) {
 
   prddat <- mod$model
   trans <- mod$trans
@@ -39,15 +40,26 @@ anlz_prd <- function(mod) {
   prd <- predict(mod, newdata = prddat, type = 'terms')
   int <- attr(prd, 'constant')
   value <- rowSums(prd) + int
-  # annvalue <- rowSums(prd[, !grepl('doy', colnames(prd)), drop = FALSE]) + int
-  
+
   # get annual trend
   out <- prddat %>% 
     dplyr::mutate(
-      # annvalue = annvalue,
       value = value, 
       trans = mod$trans
-      ) %>% 
+      )
+
+  if(annual){
+    
+    annvalue <- rowSums(prd[, !grepl('doy', colnames(prd)), drop = FALSE]) + int
+    
+    out <- out %>% 
+      dplyr::mutate(
+        annvalue = annvalue
+      )
+    
+  }  
+    
+  out <- out %>% 
     anlz_backtrans()
 
   return(out)
--- tests/testthat/test-anlz_prd.R
+++ tests/testthat/test-anlz_prd.R
@@ -8,3 +8,15 @@ test_that("Checking anlz_prd", {
   expect_equal(result, c(`1` = 2.8, `2` = 2.5, `3` = 2.3, `4` = 2.1))
   
 })
+
+test_that("Checking anlz_prd, annual = T", {
+  
+  result <- anlz_prd(mod, annual = T) %>% 
+    dplyr::pull(annvalue) %>% 
+    .[1:4] %>% 
+    round(1)
+  
+  expect_equal(result, c(`1` = 2.8, `2` = 2.5, `3` = 2.3, `4` = 2.1))
+  
+})
+
