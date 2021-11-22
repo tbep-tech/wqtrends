7dec2be11a0e6ba94b6399c78858ebe05210b881
fawda123 <mbafs2012@gmail.com>
2021-11-22 17:57:06 -0500

    adding coverage to anlz_trans test

--- tests/testthat/test-anlz_trans.R
+++ tests/testthat/test-anlz_trans.R
@@ -25,3 +25,11 @@ test_that("Checkout output of anlz_trans", {
   expect_is(result, 'data.frame')
   
 })
+
+test_that("Check warning if value is less than or equal to zero", {
+  
+  tmp <- tomod
+  tmp$value[1] <- 0
+  expect_warning(anlz_trans(tmp, trans = 'log10'))
+  
+})
