06e80d423af7489bc7b1a8ab26d70014ca63a119
fawda123 <mbafs2012@gmail.com>
2021-11-22 17:43:06 -0500

    anlz_avgseason added back, mod to anlz_prdmatrix for ave metric

--- R/anlz_prdmatrix.R
+++ R/anlz_prdmatrix.R
@@ -5,7 +5,8 @@
 #' @param mod input model object as returned by \code{\link{anlz_gam}}
 #' @param doystr numeric indicating start Julian day for extracting averages
 #' @param doyend numeric indicating ending Julian day for extracting averages
-#'
+#' @param avemat logical indicating if the prediction matrix is to be passed to \code{anlz_metseason} (default) or \code{anlz_avgseason} 
+#' 
 #' @return a \code{data.frame} with predictors to use with the fitted GAM
 #' @export
 #' 
@@ -23,25 +24,57 @@
 #'
 #' mod <- anlz_gam(tomod, trans = 'log10')
 #' anlz_prdmatrix(mod, doystr = 90, doyend = 180)
-anlz_prdmatrix <- function(mod, doystr = 1, doyend = 364){
+anlz_prdmatrix <- function(mod, doystr = 1, doyend = 364, avemat = FALSE){
 
-  # date range
-  dtrng <- range(mod$model$cont_year, na.rm = T) %>% 
-    lubridate::date_decimal() %>% 
-    as.Date %>% 
-    lubridate::year(.) 
-  dtrng <- c(as.Date(doystr, origin = paste0(dtrng[1], '-01-01')), as.Date(doyend, origin = paste0(dtrng[2], '-01-01')))
+  # create uniform matrix if metric is average
+  if(avemat){
+    
+    # date range
+    dtrng <- range(mod$model$cont_year, na.rm = T) %>% 
+      lubridate::date_decimal() %>% 
+      as.Date
+    
+    # number of days in season subset
+    numDays <- doyend - doystr + 1
+    
+    # prediction data, daily time step, subset by doystr, doyend
+    out <- data.frame(date = seq.Date(dtrng[1], dtrng[2], by = 'day')) %>% 
+      dplyr::mutate(
+        yr = lubridate::year(date), 
+        doy = lubridate::yday(date),
+        cont_year = lubridate::decimal_date(date)
+      ) %>% 
+      dplyr::filter(doy >= doystr & doy <= doyend) %>% 
+      dplyr::group_by(yr) %>% 
+      dplyr::mutate(
+        dayCounts = dplyr::n()
+      ) %>% 
+      dplyr::ungroup() %>% 
+      dplyr::filter(dayCounts == numDays) %>% 
+      dplyr::select(-dayCounts)
+    
+  }
   
-  # prediction data, daily time step, subset by doystr, doyend
-  fillData <- data.frame(date = seq.Date(dtrng[1], dtrng[2], by = 'day')) %>% 
-    dplyr::mutate(
-      yr = lubridate::year(date), 
-      doy = lubridate::yday(date),
-      cont_year = lubridate::decimal_date(date)
-    ) %>% 
-    dplyr::filter(doy >= doystr & doy <= doyend)
-  
-  out <- fillData
+  # create exact matrix if not average
+  if(!avemat){
+    
+    # date range
+    dtrng <- range(mod$model$cont_year, na.rm = T) %>% 
+      lubridate::date_decimal() %>% 
+      as.Date %>% 
+      lubridate::year(.) 
+    dtrng <- c(as.Date(doystr, origin = paste0(dtrng[1], '-01-01')), as.Date(doyend, origin = paste0(dtrng[2], '-01-01')))
+    
+    # prediction data, daily time step, subset by doystr, doyend
+    out <- data.frame(date = seq.Date(dtrng[1], dtrng[2], by = 'day')) %>% 
+      dplyr::mutate(
+        yr = lubridate::year(date), 
+        doy = lubridate::yday(date),
+        cont_year = lubridate::decimal_date(date)
+      ) %>% 
+      dplyr::filter(doy >= doystr & doy <= doyend)
+    
+  }
   
   return(out)
 
--- tests/testthat/test-anlz_prdmatrix.R
+++ tests/testthat/test-anlz_prdmatrix.R
@@ -5,3 +5,11 @@ test_that("Checking anlz_prdmatrix", {
   expect_is(result, 'data.frame')
 
 })
+
+test_that("Checking anlz_prdmatrix for anlz_avgseason", {
+  
+  result <- anlz_prdmatrix(mod, doystr = 90, doyend = 180, avemat = T) 
+  
+  expect_is(result, 'data.frame')
+  
+})
