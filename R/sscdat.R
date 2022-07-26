0a20d3f7fac4b3f73c0b3da431a030ab603b8ad8
fawda123 <mbafs2012@gmail.com>
2022-07-26 16:59:17 -0400

    added sscdat

---
+++ R/sscdat.R
@@ -0,0 +1,14 @@
+#' Daily-averaged suspended sediment concentration at Dumbarton Bridge
+#'
+#' Daily-averaged suspended sediment concentration at Dumbarton Bridge
+#'
+#' @format A \code{data.frame} object with 7335 rows and 2 columns
+#' \describe{
+#'   \item{date}{Date}
+#'   \item{ssc}{numeric, mg/L}
+#' }
+#' @family utilities
+#' 
+#' @details 
+#' Data from \code{dat_prc} object in \url{https://github.com/fawda123/SFbaytrends}
+"sscdat"
