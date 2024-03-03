5f4bfa8e8d1355f03847261288ca31b5ab681c7c
fawda123 <mbafs2012@gmail.com>
2024-03-03 12:28:44 -0500

    filling test coverage for anlz_metseason and anlz_avgseason

--- tests/testthat/test-anlz_avgseason.R
+++ tests/testthat/test-anlz_avgseason.R
@@ -19,3 +19,13 @@ test_that("Checking  error, insufficient inputs", {
   expect_error(anlz_avgseason(doystr = 90, doyend = 180))
   
 })
+
+test_that("Checking yromit", {
+  
+  result <- anlz_avgseason(mod, doystr = 90, doyend = 180, yromit = 2016)
+  
+  # excpect NA for 2016
+  expected <- na.omit(result)$yr
+  expect_equal(expected, c(2017, 2018, 2019))
+  
+})
--- tests/testthat/test-anlz_metseason.R
+++ tests/testthat/test-anlz_metseason.R
@@ -19,3 +19,13 @@ test_that("Checking  error, insufficient inputs", {
   expect_error(anlz_metseason(doystr = 90, doyend = 180, nsim = 5))
   
 })
+
+test_that("Checking yromit", {
+  
+  result <- anlz_metseason(mod, doystr = 90, doyend = 180, nsim = 5, yromit = 2016)
+   
+  # excpect NA for 2016
+  expected <- na.omit(result)$yr
+  expect_equal(expected, c(2017, 2018, 2019))
+  
+})
