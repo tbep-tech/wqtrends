dbae0d750b4f1d873c76143d39ad7f16bd5c36e2
fawda123 <mbafs2012@gmail.com>
2024-03-07 07:46:21 -0500

    fix tests for show_mettrndseason

--- tests/testthat/test-show_mettrndseason.R
+++ tests/testthat/test-show_mettrndseason.R
@@ -16,12 +16,12 @@ test_that("Checking show_mettrndseason class with cmbn as T", {
 
 test_that("show_mettrndseason throws an error if cols length is not 4 for cmbn as F", {
 
-  expect_error(show_mettrndseason(mod, cols = c('red', 'blue'), nsim = 10), "Four names or colors must be provided")
+  expect_error(show_mettrndseason(mod, fils = c('red', 'blue'), nsim = 10), "Four names or colors must be provided")
   
 })
 
 test_that("show_mettrndseason throws an error if cols length is not 3 for cmbn as T", {
   
-  expect_error(show_mettrndseason(mod, cols = c('red', 'blue'), cmbn = T, nsim = 10), "Three names or colors must be provided")
+  expect_error(show_mettrndseason(mod, fils = c('red', 'blue'), cmbn = T, nsim = 10), "Three names or colors must be provided")
   
 })
