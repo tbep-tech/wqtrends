8b3902f390fbb44b527bc1db6fa6fb84876bfbee
fawda123 <mbafs2012@gmail.com>
2025-08-19 16:06:08 -0400

    completing tests for show_prdseries yromit

--- tests/testthat/test-show_prdseries.R
+++ tests/testthat/test-show_prdseries.R
@@ -8,9 +8,36 @@ test_that("Checking show_prdseries class", {
 
 test_that("Checking show_series yromit argument works", {
   
+  # normal year omit
   result <- show_prdseries(mod, ylab = 'Chlorophyll-a (ug/L)', yromit = 2016)
   
   chk <- result$data[result$data$yr == 2016, 'value',]
   expect_true(all(is.na(chk)))
+  
+  # year omit outside of range
+  result <- show_prdseries(mod, ylab = 'Chlorophyll-a (ug/L)', yromit = 2014)
+  
+  chk <- result$data[result$data$yr == 2014, 'value',]
+  expect_true(length(chk) == 0)
+
+  # edge case model full missing years
+  tomod <- subset(rawdat, rawdat$station == 34 & rawdat$param == 'chl' & yr > 2014)
+  tomod <- subset(tomod, tomod$cont_year < 2016 | tomod$cont_year >= 2019)
+  mod <- anlz_gam(tomod, trans = 'log10')
+
+  result <- show_prdseries(mod, ylab = 'Chlorophyll-a (ug/L)', yromit = 2017)
+
+  chk <- na.omit(result$data[result$data$yr == 2017, 'value',])
+  expect_true(length(chk) == 0)
+
+  # edge case model partial missing years
+  tomod <- subset(rawdat, rawdat$station == 34 & rawdat$param == 'chl' & yr > 2014)
+  tomod <- subset(tomod, tomod$cont_year < 2016 | tomod$cont_year >= 2018.5)
+  mod <- anlz_gam(tomod, trans = 'log10')
+
+  result <- show_prdseries(mod, ylab = 'Chlorophyll-a (ug/L)', yromit = 2017)
+
+  chk <- na.omit(result$data[result$data$yr == 2018, 'value',])
+  expect_true(length(chk) > 0)
   
 })
